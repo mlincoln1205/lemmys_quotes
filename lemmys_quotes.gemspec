c2a93a4da257b5dcfd36ef3258b1aeeda89f1c89
mlincoln1205 <mlincoln1205@gmail.com>
2023-01-15 15:46:55 +0100

    First Commit

---
+++ lemmys_quotes.gemspec
@@ -0,0 +1,7 @@
+Gem::Specification.new do |s|
+  s.authors = ['Lincoln Marcolongo']
+  s.files = Dir['lib/**/*.rb']
+  s.name = 'lemmys_quotes'
+  s.summary = 'A gem to give you beautiful quotes straight from hell by Lemmy Kilmster'
+  s.version = '0.0.1'
+end
---
+++ lib/lemmys_quotes.rb
@@ -0,0 +1,12 @@
+class String
+  QUOTES = [
+    "Born to lose. Live to win.",
+    "If you didn't do anything that wasn't good for you it would be a very dull life. What are you gonna do? Everything that is pleasant in life is dangerous. Have you noticed that? I'd like to find the bastard that thought that one up.",
+    "That was a great time, the summer of '71 - I can't remember it, but I'll never forget it!",
+    "If there is a God he hasn’t been paying attention. He should retire and hand over to a younger man, because he’s making a real bollocks of everything.",
+    "We are Motörhead. And we play rock and roll.",
+    "We were a blues band, really. Although we played it at a thousand miles an hour, it was recognizable as blues – at least to us it was; probably it wasn’t to anybody else."
+  ].freeze
+
+  def lemmy_says; QUOTES.sample end
+end
