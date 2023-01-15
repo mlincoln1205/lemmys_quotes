7271911e1d2c3f683349ba42d5331272d11b0423
mlincoln1205 <mlincoln1205@gmail.com>
2023-01-15 16:36:26 +0100

    Adding quotes

--- lib/lemmys_quotes.rb
+++ lib/lemmys_quotes.rb
@@ -5,6 +5,7 @@ class String
     "That was a great time, the summer of '71 - I can't remember it, but I'll never forget it!",
     "If there is a God he hasn’t been paying attention. He should retire and hand over to a younger man, because he’s making a real bollocks of everything.",
     "We are Motörhead. And we play rock and roll.",
+    "People don’t become better when they’re dead; you just talk about them as if they are.People don’t become better when they’re dead; you just talk about them as if they are.",
     "We were a blues band, really. Although we played it at a thousand miles an hour, it was recognizable as blues – at least to us it was; probably it wasn’t to anybody else."
   ].freeze
 
